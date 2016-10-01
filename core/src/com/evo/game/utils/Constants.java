3476622fd4069789d0bf8fb02fe0ad0962e63073
jaden71 <jadenyjw@gmail.com>
2016-09-30 21:44:21 -0400

    More implementation

--- core/src/com/evo/game/utils/Constants.java
+++ core/src/com/evo/game/utils/Constants.java
@@ -15,9 +15,7 @@ public class Constants {
     public static final float GROUND_Y = 0;
 
     public static final float GROUND_HEIGHT = 2f;
-
     public static final float SPAWN_RADIUS = 10f;
-    
     public static final float RUNNER_X = 2;
     public static final float RUNNER_Y = GROUND_Y + GROUND_HEIGHT;
     public static final float RUNNER_WIDTH = 1f;
--- core/src/com/evo/networks/Network.java
+++ core/src/com/evo/networks/Network.java
@@ -3,6 +3,8 @@ package com.evo.networks;
 import com.badlogic.gdx.utils.Array;
 
 public class Network extends Array<Array<Float>> {
+	
+	private Array<Array<Float>> weights;
 
 	public Network() {
 	  
@@ -10,6 +12,24 @@ public class Network extends Array<Array<Float>> {
 	
 	public void setWeights(Array<Float> gene){
 		
+		for (int x = 0; x < 25; x++){
+			weights.get(0).set(x ,gene.get(x));
+		}
+		for (int x = 25; x < 50; x++){
+			weights.get(1).set(x ,gene.get(x));
+		}
+		for (int x = 50; x < 64; x++){
+			weights.get(2).set(x ,gene.get(x));
+		}
+	
+	}
+	
+	
+	
+	public Array<Float> compute(Array<Float> inputs){
+		
+		
+		return new Array<Float>(); //temp
 	}
 
 
